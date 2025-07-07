fce3199a9c4c350f8771b3af2021992ada25b18d
Derrick <iga00257@gmail.com>
2025-07-07 22:19:43 +0800

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { isrMemoryCacheSize: 0 },
   images: {
     domains: [
       'images.unsplash.com',
